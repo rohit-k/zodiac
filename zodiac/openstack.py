7426477ddcb6b50e79f58473602aef3666020459
Daryl Walleck <daryl.walleck@rackspace.com>
2011-09-18 15:32:00 -0500

    Removed credentials from Manager class

--- zodiac/openstack.py
+++ zodiac/openstack.py
@@ -3,6 +3,4 @@ from zodiac.services.nova.json import servers_json_client
 class Manager(object):
 
     def __init__(self):
-        self.servers_client = servers_json_client.ServersClient('dwalleck', 
-            'aacfe0a3-d0b6-4ecc-a469-912a6a629e6d', 
-            'http://alpha.ord.servers.api.rackspacecloud.com:8774/v1.1')
+        self.servers_client = servers_json_client.ServersClient(user, key, auth_url)
