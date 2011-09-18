7173ef98191ad881a4cabb0cb3f563f100493d6d
Daryl Walleck <daryl.walleck@rackspace.com>
2011-09-18 16:23:09 -0500

    Added attribute tag for tests, added some content to README

--- zodiac/openstack.py
+++ zodiac/openstack.py
@@ -3,4 +3,9 @@ from zodiac.services.nova.json import servers_json_client
 class Manager(object):
 
     def __init__(self):
+        """
+        Top level manager for all Openstack APIs
+        TODO: Read user, key, auth_url from configuration file
+        """
+        
         self.servers_client = servers_json_client.ServersClient(user, key, auth_url)
--- zodiac/tests/test_servers.py
+++ zodiac/tests/test_servers.py
@@ -1,3 +1,4 @@
+from nose.plugins.attrib import attr
 from zodiac import openstack
 import unittest2 as unittest
 
@@ -11,13 +12,15 @@ class ServersTest(unittest.TestCase):
     @classmethod
     def tearDownClass(cls):
         pass
-        
+    
+    @attr(type='positive')
     def test_create_delete_server(self):
         server = self.client.create_server('clienttest', 2, 1)
         self.id = server['server']['id']
         self.client.wait_for_server_status(self.id, 'ACTIVE')
         self.client.delete_server(self.id)
-        
+    
+    @attr(type='positive')
     def test_update_server(self):
         server = self.client.create_server('clienttest', 2, 1)
         self.id = server['server']['id']
